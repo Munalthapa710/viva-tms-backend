6216b4828588bfd59aaf5ced6c2b173a5c93b1d8
Munalthapa710 <thapamunal710@gmail.com>
2026-01-04 16:00:37 +0545

    okkkk

--- backend/app.py
+++ backend/app.py
@@ -483,6 +483,7 @@ def delete_work_todo(id):
 
     return jsonify({"message": "Work todo deleted successfully"})
 
+
 # ================= RUN =================
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
